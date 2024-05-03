d5f03853e09d036e2282b039186e12d3c4e73113
ManuelJuliao <manuelcjuliao@gmail.com>
2024-05-03 21:37:34 +0100

    First commit, project initialized. Spring Boot + Java

---
+++ src/test/java/com/example/SnakesNLadders/SnakesNLaddersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SnakesNLadders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakesNLaddersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
